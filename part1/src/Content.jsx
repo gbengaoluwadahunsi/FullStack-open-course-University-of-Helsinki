595e3725f38856f66a8bff011b24d90af5f16172
Gbenga <gbengaoluwadahunsi01@gmail.com>
2023-11-28 17:11:55 +0800

    Exercise 1.1 -1.2

--- part1/src/Content.jsx
+++ part1/src/Content.jsx
@@ -1,19 +1,18 @@
+import Part from "./Part"
 
+const Content = () => {
 
-const Content = (props) => {
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
   return (
     <>
-      
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
-
+     <Part  part =  {part1}  exercises= {exercises1}/>
+     <Part  part =  {part2}  exercises= {exercises2}/>
+     <Part  part =  {part3}  exercises= {exercises3} />      
     </>
   )
 }
---
+++ part1/src/Part.jsx
@@ -0,0 +1,13 @@
+
+
+const Part = (props) => {
+  return (
+    <>
+     <p>
+        {props.part} {props.exercises}
+      </p>
+    </>
+  )
+}
+
+export default Part
