6061c65d0a00b785d28c20f87d594bcedf8873ca
Gbenga <gbengaoluwadahunsi01@gmail.com>
2023-12-03 23:49:34 +0800

    Add solution to exercise part2.1-2.5

--- part2/part2-ExerciseSolutions2.1-2.5/src/App.jsx
+++ part2/part2-ExerciseSolutions2.1-2.5/src/App.jsx
@@ -1,43 +1,64 @@
 
 import Content from './Content'
-import Total from './Total'
-import Header from './Header'
+
+
 
 
 
 const App = () => {
    
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      { id: 0,
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      { id: 1,
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      { id : 2,
-        name: 'State of a component',
-        exercises: 14
-      },
-      { id : 3,
-        name: 'Redux',
-        exercises: 11
-      },
-
-    ]
-  }
-
   
-   
+    const courses = [
+      {
+        name: 'Half Stack application development',
+        id: 1,
+        parts: [
+          {
+            name: 'Fundamentals of React',
+            exercises: 10,
+            id: 1
+          },
+          {
+            name: 'Using props to pass data',
+            exercises: 7,
+            id: 2
+          },
+          {
+            name: 'State of a component',
+            exercises: 14,
+            id: 3
+          },
+          {
+            name: 'Redux',
+            exercises: 11,
+            id: 4
+          }
+        ]
+      }, 
+      {
+        name: 'Node.js',
+        id: 2,
+        parts: [
+          {
+            name: 'Routing',
+            exercises: 3,
+            id: 1
+          },
+          {
+            name: 'Middlewares',
+            exercises: 7,
+            id: 2
+          }
+        ]
+      }
+    ]
+  
+  
   return  (
     <>
-     <Header  course = {course.name}/>
-     <Content parts = {course.parts} />
-     <Total  parts = {course.parts}/>
-               
+    <h1>Web development curriculum</h1>
+     <Content courses = {courses} />
+                   
     </>
   )
 }
--- part2/part2-ExerciseSolutions2.1-2.5/src/Content.jsx
+++ part2/part2-ExerciseSolutions2.1-2.5/src/Content.jsx
@@ -1,15 +1,50 @@
-import  Part from './Part'
 
 
-const Content = (props) => {
+const Part = ({name , exercises}) => {
+  return (
+    <>
+     <p>
+        {name} {exercises}
+      </p>
+    </>
+  )
+}
 
+
+
+const Total = ({exercises}) => {
+  
+  return (
+    <>
+      <h4> total of {exercises.reduce((a,c) =>  a+c.exercises , 0)} exercises</h4>
+    </>
+  )
+}
+
+
+
+
+
+
+const Content = ({courses}) => {
   
     return (
       <>
-       <Part   name = {props.parts[0].name}  exercises= {props.parts[0].exercises} />
-       <Part  name = {props.parts[1].name}  exercises= {props.parts[1].exercises}/>
-       <Part  name =  {props.parts[2].name}  exercises= {props.parts[2].exercises} />   
-       <Part  name =  {props.parts[3].name}  exercises= {props.parts[3].exercises} />    
+        <section>
+          <h3>{courses[0].name}</h3>
+          <Part  name = {courses[0].parts[0].name}  exercises= {courses[0].parts[0].exercises} />
+          <Part  name = {courses[0].parts[1].name}  exercises= {courses[0].parts[1].exercises}/>
+          <Part  name = {courses[0].parts[2].name}  exercises= {courses[0].parts[2].exercises} />   
+          <Part  name = {courses[0].parts[3].name}  exercises= {courses[0].parts[3].exercises} /> 
+           <Total  exercises = {courses[0].parts}/>   
+        </section>
+
+        <section>
+          <h3>{courses[1].name}</h3>
+          <Part  name = {courses[1].parts[0].name}  exercises= {courses[1].parts[0].exercises} />
+          <Part  name = {courses[1].parts[1].name}  exercises= {courses[1].parts[1].exercises}/>
+          <Total  exercises = {courses[1].parts}/>    
+        </section>
       </>
     )
   }
